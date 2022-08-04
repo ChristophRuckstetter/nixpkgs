4580860f1142e3921a7c4f5722b1504e3f71890b
Christoph Ruckstetter <christoph.ruckstetter@weinig.com>
2022-08-04 10:28:55 +0200

    change license of xdg-utils to mit

--- pkgs/tools/X11/xdg-utils/default.nix
+++ pkgs/tools/X11/xdg-utils/default.nix
@@ -63,7 +63,7 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     homepage = "https://www.freedesktop.org/wiki/Software/xdg-utils/";
     description = "A set of command line tools that assist applications with a variety of desktop integration tasks";
-    license = if mimiSupport then licenses.gpl2 else licenses.free;
+    license = licenses.mit;
     maintainers = [ maintainers.eelco ];
     platforms = platforms.all;
   };
